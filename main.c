52980eb7a0410d73d281e69a5c08dcfd5c043085
fabio <eng.fabio.salvatori@gmail.com>
2025-05-23 17:54:57 +0200

    handling exit command

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
+#define _POSIX_SOURCE
 #include <fcntl.h>
+#include <signal.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,6 +26,11 @@ void print_parsed_input_command(char *arguments[]) {
     j++;
   }
   putchar('\n');
+}
+
+static void exit_function() {
+  write(STDOUT_FILENO, "exiting minimal shell\n", 22);
+  exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char *argv[]) {
@@ -56,8 +63,11 @@ int main(int argc, char *argv[]) {
       arguments[i - 1][strlen(arguments[i - 1]) - 1] = '\0';
       arguments[i] = NULL;
 
-
       if (arguments[0] != NULL) {
+        if (strcmp(arguments[0], "exit") == 0) {
+          kill(getppid(), SIGINT);
+        }
+
         /*print_parsed_input_command(arguments);*/
         execvp(arguments[0], arguments);
       }
@@ -65,8 +75,15 @@ int main(int argc, char *argv[]) {
 
     } else {
       // parent process
+
+      if (signal(SIGINT, exit_function) == SIG_ERR) {
+        fputs("An error occurred while setting a signal handler.\n", stderr);
+        return EXIT_FAILURE;
+      }
+
       int return_code;
       return_code = waitpid(process_id, NULL, 0);
+
       write(STDOUT_FILENO, "msh$ ", 5);
       memset(buf, '\0', MAX_SHELL_INPUT_LEN);
       /*printf("parent process - pid : %d\n", getpid());*/
